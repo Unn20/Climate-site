304e64bd4e00c378bc5d35a1d946b35fd8490d79
Kamil Kowalczyk <kamkow10@wp.pl>
2020-10-27 17:48:19 +0100

    Create home-page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { CountersComponent } from './components/counters/counters.component';
+import { FiguresComponent } from './components/figures/figures.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NewsHeaderComponent } from './components/news-header/news-header.component';
+import { PollutionEffectsComponent } from './components/pollution-effects/pollution-effects.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomePageComponent,
+        CountersComponent,
+        FiguresComponent,
+        FooterComponent,
+        NewsHeaderComponent,
+        PollutionEffectsComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
