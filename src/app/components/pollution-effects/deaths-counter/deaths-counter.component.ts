2d41d0529698d8944938d796d5b1c0f51699d7ac
Kamil Kowalczyk <kamkow1998@gmail.com>
2020-11-06 19:13:27 +0000

    Add pollution effects and footer

---
+++ src/app/components/pollution-effects/deaths-counter/deaths-counter.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {CountUp} from 'countup.js';
+
+@Component({
+  selector: 'app-deaths-counter',
+  templateUrl: './deaths-counter.component.html',
+  styleUrls: ['./deaths-counter.component.scss']
+})
+export class DeathsCounterComponent implements OnInit {
+
+    private distanceToStartFadeIn = 500;
+    private yearlyDeathsCausedByPollution = 6000000;
+    private countUp: CountUp;
+    public counterStarted = false;
+    public counterFinished = false;
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.countUp = new CountUp(
+            'countUp',
+            this.yearlyDeathsCausedByPollution,
+            {
+                startVal: this.yearlyDeathsCausedByPollution - 1000,
+                duration: 1.5,
+                separator: ' '
+            }
+        );
+    }
+
+    public onScroll(container: HTMLDivElement): void {
+        if (!this.counterStarted && (document.scrollingElement.scrollTop > container.offsetTop - this.distanceToStartFadeIn)) {
+            this.countUp.start();
+            this.counterStarted = true;
+            setTimeout(() => {
+                this.counterFinished = true;
+            }, 1700);
+        }
+    }
+
+}
