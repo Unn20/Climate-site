76c3359a5c22efc317944864a97974ff564f933e
Unn20 <maciej.leszczyk@student.put.poznan.pl>
2020-12-06 14:48:44 +0100

    Backend URL now is inside enviroment.ts and enviroment.prod.ts files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
     production: false,
+    backend: {
+        url: 'http://3.135.244.176:3000/'
+    },
     contentful: {
         space: '01yn6lkn00ay',
         accessToken: 'afYgrxTYcjw2VWFGHS_QT40Ri1FMnucKLGwuGUgTDn0'
