c058b13f0859664d690fac719254827b1c9995d6
Unn20 <maciej.leszczyk@student.put.poznan.pl>
2020-12-08 19:56:26 +0100

    Changing logo as response to site resize event + unit tests

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,34 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, HostListener, Output} from '@angular/core';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-    public list: Array<string> =  ['1', '2', '3'];
+export class NavbarComponent implements AfterViewInit {
+    public innerWidth: any;
 
-    // TODO: dodac responsywnosc
+    public logoPath = 'assets/img/icons/logo3.png';
+
+    ngAfterViewInit(): void {
+        this.innerWidth = window.innerWidth;
+        this.setLogo();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event): void {
+        this.innerWidth = window.innerWidth;
+        this.setLogo();
+    }
+
+    setLogo(): void {
+        if (this.innerWidth >= 700) {
+            this.logoPath = 'assets/img/icons/logo3.png';
+        } else if (this.innerWidth >= 550) {
+            this.logoPath = 'assets/img/icons/logo2.png';
+        } else {
+            // TODO: refaktor jak bedzie trzeba
+            this.logoPath = 'assets/img/icons/logo2.png';
+        }
+    }
 }
