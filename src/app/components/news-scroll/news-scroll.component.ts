9e389d76f1c2e17b4906f032a3cbae177d2407e6
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-12-12 22:44:00 +0100

    Added responsive scaling for news-scroll. Scaling works for changed width and height

--- src/app/components/news-scroll/news-scroll.component.ts
+++ src/app/components/news-scroll/news-scroll.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Renderer2, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {interval, Observable} from 'rxjs';
 import {ArticleService} from '../../services/article.service';
 import {Article} from '../../models/article';
@@ -8,20 +8,62 @@ import {ScrollViewComponent} from '@progress/kendo-angular-scrollview';
 @Component({
     selector: 'app-news-scroll',
     templateUrl: './news-scroll.component.html',
-    styleUrls: ['./news-scroll.component.scss']
+    styleUrls: ['./news-scroll.component.scss'],
 })
 export class NewsScrollComponent implements OnInit, OnDestroy {
     @ViewChild('myScroll') private myScrollView: ScrollViewComponent;
     public articles: Article[] = [];
     public width = '100%';
-    public height = '600px';
+    public height = '800px';
+    public maxHeight = 800;
+    public minHeight = 200;
     public articleHover = false;
     public subscription = null;
+    private readonly listenFunc: () => void;
+    private lastWidth = 0;
+    private lastHeight = 0;
 
-    constructor(public articleService: ArticleService) {
+    constructor(private renderer: Renderer2, public articleService: ArticleService) {
         articleService.getArticles().then(articles => {
             this.articles = articles;
         });
+        this.lastHeight = window.innerHeight;
+        this.lastWidth = window.innerWidth;
+        this.updateHeight();
+        this.listenFunc = renderer.listen('window', 'resize', () => this.updateHeight());
+    }
+
+    public updateHeight(): void {
+        let widthChanged = false;
+        if (this.lastWidth !== window.innerWidth){
+            this.lastWidth = window.innerWidth;
+            widthChanged = true;
+        } else {
+            this.lastHeight = window.innerHeight;
+        }
+        const width = window.innerWidth;
+        if (widthChanged) {
+            if (width < 1200) {
+                if (this.maxHeight - (1200 - width) > 200) {
+                    this.height = `${this.maxHeight - (1200 - width)}px`;
+                } else {
+                    this.height = `${this.minHeight}px`;
+                }
+            } else {
+                this.height = `${this.maxHeight}px`;
+            }
+        } else {
+            const heightDifference = window.innerHeight - +this.height.slice(0, -2);
+            if (heightDifference < 70) {
+                if (window.innerHeight > 200){
+                    this.height = `${window.innerHeight - 70}px`;
+                } else {
+                    this.height = `${this.minHeight}px`;
+                }
+            } else {
+                this.height = `${this.maxHeight}px`;
+            }
+        }
     }
 
     private next_article(): void {
@@ -42,6 +84,7 @@ export class NewsScrollComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         this.subscription.unsubscribe();
+        this.listenFunc();
     }
 
 }
