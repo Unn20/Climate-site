e7d3b14138cb029b3c7e4d7777d9900bdef2f3d5
Krzysztof Charlikowski <krajzys@o2.pl>
2020-12-12 20:31:38 +0100

    Replace about-us selector

--- src/app/components/about-us-page/about-us-page.component.ts
+++ src/app/components/about-us-page/about-us-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-page',
+  selector: 'app-about-us-page',
   templateUrl: './about-us-page.component.html',
   styleUrls: ['./about-us-page.component.scss']
 })
