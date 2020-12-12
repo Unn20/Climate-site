c28862151e92aee9826a1a3243a7e1aa5d44333f
Krzysztof Charlikowski <krajzys@o2.pl>
2020-12-12 20:23:09 +0100

    Refactor contact-page into about-us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from './components/home-page/home-page.component';
 import {ArticlePageComponent} from './components/article-page/article-page.component';
-import {ContactPageComponent} from './components/contact-page/contact-page.component';
+import {AboutUsPageComponent} from './components/about-us/about-us-page.component';
 import {PrivacyPolicyPageComponent} from './components/privacy-policy-page/privacy-policy-page.component';
 import {ArticleListPageComponent} from './components/article-list-page/article-list-page.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
@@ -11,7 +11,7 @@ const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomePageComponent},
     {path: 'article/:articleId', component: ArticlePageComponent},
-    {path: 'contact', component: ContactPageComponent},
+    {path: 'about', component: AboutUsPageComponent},
     {path: 'privacy', component: PrivacyPolicyPageComponent},
     {path: 'articles', component: ArticleListPageComponent},
     {path: '**', component: PageNotFoundComponent},
