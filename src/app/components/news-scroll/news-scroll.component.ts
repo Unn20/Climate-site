fafc5e88cc15d5beb937f5357cd7a1ec209e2917
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-12-15 16:27:29 +0100

    Fixed null value for subscription on declaration, added countdown reset when scrolling news, successfuly added endless parameter == true so that the user can loop through news -> last and first news have arrows to move to the opposite news

--- src/app/components/news-scroll/news-scroll.component.ts
+++ src/app/components/news-scroll/news-scroll.component.ts
@@ -1,16 +1,16 @@
 import {AfterViewInit, Component, Renderer2, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {interval, Observable} from 'rxjs';
+import {interval, Observable, Subject, Subscribable, Subscription, timer} from 'rxjs';
 import {ArticleService} from '../../services/article.service';
 import {Article} from '../../models/article';
 import {ScrollViewComponent} from '@progress/kendo-angular-scrollview';
-
+import {startWith, switchMap} from 'rxjs/operators';
 
 @Component({
     selector: 'app-news-scroll',
     templateUrl: './news-scroll.component.html',
     styleUrls: ['./news-scroll.component.scss'],
 })
-export class NewsScrollComponent implements OnInit, OnDestroy {
+export class NewsScrollComponent implements OnInit, OnDestroy, AfterViewInit {
     @ViewChild('myScroll') private myScrollView: ScrollViewComponent;
     public articles: Article[] = [];
     public width = '100%';
@@ -18,22 +18,29 @@ export class NewsScrollComponent implements OnInit, OnDestroy {
     public maxHeight = 800;
     public minHeight = 200;
     public articleHover = false;
-    public subscription = null;
-    private readonly listenFunc: () => void;
     private lastWidth = 0;
     private lastHeight = 0;
+    public autoScrollingSubscription: Subscription;
+    private readonly heightListener: () => void;
+    private scrollListener: () => void;
+    private scrollTimer: Observable<any>;
+    private resetTimer = new Subject();
 
     constructor(private renderer: Renderer2, public articleService: ArticleService) {
         articleService.getArticles().then(articles => {
-            this.articles = articles;
+            this.articles = articles;  // TODO: sort by id take up to 5
         });
         this.lastHeight = window.innerHeight;
         this.lastWidth = window.innerWidth;
         this.updateHeight();
-        this.listenFunc = renderer.listen('window', 'resize', () => this.updateHeight());
+        this.heightListener = renderer.listen('window', 'resize', () => this.updateHeight());
+        this.scrollTimer = this.resetTimer.pipe(
+            startWith( 0),
+            switchMap(() => timer(5000, 5000))
+        );
     }
 
-    public updateHeight(): void {
+    private updateHeight(): void {
         let widthChanged = false;
         if (this.lastWidth !== window.innerWidth){
             this.lastWidth = window.innerWidth;
@@ -67,9 +74,7 @@ export class NewsScrollComponent implements OnInit, OnDestroy {
     }
 
     private next_article(): void {
-        const source = interval(5000);
-        this.subscription = source.subscribe(_ => {
-            console.log(this.myScrollView.activeIndex);
+        this.autoScrollingSubscription = this.scrollTimer.subscribe(_ => {
             if (this.myScrollView.activeIndex + 1 === this.articles.length) {
                 this.myScrollView.pageChange(0);
             } else {
@@ -78,13 +83,31 @@ export class NewsScrollComponent implements OnInit, OnDestroy {
         });
     }
 
-    ngOnInit(): void{
+    public refreshScrollTimer(event): void {
+        this.resetTimer.next(void 0);  // true
+    }
+
+    ngOnInit(): void {
         this.next_article();
     }
 
     ngOnDestroy(): void {
-        this.subscription.unsubscribe();
-        this.listenFunc();
+        if (this.autoScrollingSubscription) {
+            this.autoScrollingSubscription.unsubscribe();
+        }
+        if (this.heightListener) {
+            this.heightListener();
+        }
+        if (this.scrollListener) {
+            this.scrollListener();
+        }
     }
 
+    ngAfterViewInit(): void {
+        for(let i = 0; i < this.articles.length; i++) {
+            this.myScrollView.next();
+        }
+        this.myScrollView.pageChange(0);
+        this.myScrollView.endless = true;
+    }
 }
