146beae8d63393a1e45412a1d3eec150cada6192
Krzysztof Charlikowski <krajzys@o2.pl>
2020-12-17 00:11:04 +0100

    Replace url with enviroment link

--- src/app/services/counters.service.ts
+++ src/app/services/counters.service.ts
@@ -1,17 +1,18 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountersService {
-  url = 'http://3.135.244.176:3000/api/counters';
+  // Do usunięcia po zaimplementowaniu i zmergowaniu pull requesta
   urlDev = 'http://localhost:3000/api/counters';
 
   constructor(private httpClient: HttpClient) { }
 
   getCountersData(): Observable<number[]> {
-      return this.httpClient.get<number[]>(this.urlDev);
+      return this.httpClient.get<number[]>(environment.backend.url);
   }
 }
