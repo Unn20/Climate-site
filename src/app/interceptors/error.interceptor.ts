1ec65ea384b7cb1b3394f6c3ca8b4593e5e74220
Unn20 <maciej.leszczyk@student.put.poznan.pl>
2021-01-10 14:25:16 +0100

    Caching is working!

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -6,17 +6,31 @@ import {
     HttpErrorResponse,
     HttpInterceptor, HttpResponse
 } from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
 import {catchError, finalize, retry, tap} from 'rxjs/operators';
-import {Router} from '@angular/router';
-import { LoadingPageService } from '../services/loading-page.service';
+import {LoadingPageService} from '../services/loading-page.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private router: Router, private loadingPage: LoadingPageService) {
+    private cache = new Map<string, any>();
+
+    constructor(private loadingPage: LoadingPageService) {
     }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        if (request.method !== 'GET') {
+            return next.handle(request).pipe(
+                catchError((error: HttpErrorResponse) => {
+                    return this.handleError(error);
+                })
+            );
+        }
+
+        const cachedResponse = this.cache.get(request.url);
+        if (cachedResponse) {
+            return of(cachedResponse);
+        }
+
         this.loadingPage.incrementItemsLoading();
         return next.handle(request)
             .pipe(
@@ -24,23 +38,26 @@ export class ErrorInterceptor implements HttpInterceptor {
                 tap((event: HttpEvent<any>) => {
                     if (event instanceof HttpResponse) {
                         this.loadingPage.setError(false);
+                        this.cache.set(request.url, event);
                     }
                 }),
                 catchError((error: HttpErrorResponse) => {
-                    let errorMessage = '';
-                    if (error.error instanceof ErrorEvent) {
-                        // client-side error
-                        errorMessage = `Error: ${error.error.message}`;
-                    } else {
-                        // server-side error
-                        errorMessage = `Error Status: ${error.status}\nMessage: ${error.message}`;
-                    }
-                    this.loadingPage.setError(true);
-                    return throwError(errorMessage);
+                    return this.handleError(error);
                 }),
                 finalize(() => {
                     this.loadingPage.decrementItemsLoading();
                 })
             );
+    }
+
+    handleError(error: HttpErrorResponse): Observable<never> {
+        let errorMessage: string;
+        if (error.error instanceof ErrorEvent) {
+            errorMessage = `Error: ${error.error.message}`;
+        } else {
+            errorMessage = `Error Status: ${error.status}\nMessage: ${error.message}`;
+        }
+        this.loadingPage.setError(true);
+        return throwError(errorMessage);
     }
 }
